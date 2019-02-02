994d753bc9bdb5671a85afa0bbbcc38400f2359e
Serkant <fidan.serkant@gmail.com>
2019-02-02 13:25:42 -0500

    .gitignore added pages extentd testbase deleted

--- src/test/java/com/briteErp/pages/PipelinePage.java
+++ src/test/java/com/briteErp/pages/PipelinePage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class PipelinePage extends TestBase {
+public class PipelinePage  {
 
     public PipelinePage(){
         PageFactory.initElements(Driver.getDriver(),this);
